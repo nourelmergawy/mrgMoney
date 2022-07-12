85c77b91fe5bb2a5a60037a36925864e4049a66a
nourelmergawy <nouralmergawy@gmail.com>
2022-07-12 15:59:53 +0200

    fixes bugs2

--- app/src/main/java/com/mrg/mrgmoney/MainActivity.kt
+++ app/src/main/java/com/mrg/mrgmoney/MainActivity.kt
@@ -2,9 +2,12 @@ package com.mrg.mrgmoney
 
 import CoinListAdapter
 import android.os.Bundle
+import android.text.TextUtils
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModelProvider
@@ -12,7 +15,6 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.mrg.mrgmoney.DataBase.Coin
 import com.mrg.mrgmoney.ViewModel.CoinViewModel
-import java.util.*
 
 class MainActivity : AppCompatActivity() {
     lateinit var gainBtn : Button
@@ -21,6 +23,8 @@ class MainActivity : AppCompatActivity() {
     lateinit var total : TextView
     private lateinit var coinViewModel: CoinViewModel
     private lateinit var coinListAdapter: CoinListAdapter
+    lateinit var list : LiveData<List<Coin>>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -35,13 +39,35 @@ class MainActivity : AppCompatActivity() {
         coinListAdapter = CoinListAdapter(this) { coin, i ->
             recyclerView.adapter = coinListAdapter
             coinViewModel = ViewModelProvider(this).get(coinViewModel::class.java)
-           var it :LiveData<List<Coin>>
-           println(coinViewModel.getAllCoin())
-           //coinListAdapter.setCoins()
+           coinViewModel.getAllCoin()
 
-
+            gainBtn.setOnClickListener(View.OnClickListener {
+                var amount : String = addMoney.toString()
+                insertDataToDataBase(amount.toInt(),"gain")
+            })
 
         }
 
     }
+    private fun insertDataToDataBase(amount : Int,type : String) {
+        //val totalMoney: String = addMoney.getText().toString()
+        //val totalMoneyInt : Int = Integer.parseInt(totalMoney)
+        //total.setText(totalMoney)
+        if(inputCheck(amount)){
+            val coin = Coin(0,"fdfd",type,amount.toInt(),0)
+            coinViewModel.addCoin(coin)
+
+           // coinListAdapter.setCoins(list)
+            Toast.makeText(this,"successfully",Toast.LENGTH_SHORT)
+        }else{
+            Toast.makeText(this,"faild",Toast.LENGTH_SHORT)
+
+        }
+
+    }
+    private fun inputCheck( money : Int): Boolean {
+        return !(TextUtils.isEmpty(money.toString()))
+    }
 }
+
+
