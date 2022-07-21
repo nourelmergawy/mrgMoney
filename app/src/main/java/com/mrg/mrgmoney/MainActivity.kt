d9e18fadfc2cfd0b60059eb8d973533659cd01bf
nourelmergawy <nouralmergawy@gmail.com>
2022-07-21 15:03:13 +0200

    fixes bugs6 - getDate() working -spendBtn working

--- app/src/main/java/com/mrg/mrgmoney/MainActivity.kt
+++ app/src/main/java/com/mrg/mrgmoney/MainActivity.kt
@@ -2,6 +2,7 @@ package com.mrg.mrgmoney
 
 import CoinListAdapter
 import android.content.ContentValues.TAG
+import android.icu.text.SimpleDateFormat
 import android.os.Build
 import android.os.Bundle
 import android.text.TextUtils
@@ -12,7 +13,6 @@ import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import androidx.annotation.RequiresApi
-import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -21,12 +21,14 @@ import androidx.recyclerview.widget.RecyclerView
 import com.mrg.mrgmoney.DataBase.Coin
 import com.mrg.mrgmoney.ViewModel.CoinViewModel
 import java.time.LocalDateTime
+import java.util.*
+import kotlin.collections.ArrayList
 
 class MainActivity : AppCompatActivity() , CoinListAdapter.DeleteInterface {
     lateinit var gainBtn : Button
     lateinit var spendBtn : Button
     lateinit var addMoney : EditText
-    lateinit var total : TextView
+    lateinit var tvTotal : TextView
     private lateinit var coinViewModel: CoinViewModel
     private lateinit var coinListAdapter: CoinListAdapter
 
@@ -38,7 +40,8 @@ class MainActivity : AppCompatActivity() , CoinListAdapter.DeleteInterface {
         gainBtn = findViewById(R.id.gain_btn)
         spendBtn = findViewById(R.id.spend_btn)
         addMoney = findViewById(R.id.add_money)
-        total = findViewById(R.id.total_money)
+        tvTotal = findViewById(R.id.total_money)
+
         // getting the recyclerview by its id
         val recyclerview = findViewById<RecyclerView>(R.id.recyclerview)
 
@@ -54,23 +57,27 @@ class MainActivity : AppCompatActivity() , CoinListAdapter.DeleteInterface {
 
         // Setting the Adapter with the recyclerview
         recyclerview.adapter = adapter
+
         // on below line we are
         // initializing our view modal.
         coinViewModel = ViewModelProvider(this, ViewModelProvider.AndroidViewModelFactory.getInstance(application)).get(CoinViewModel::class.java)
         coinViewModel.allCoins.observe(this, Observer { list ->
             list?.let {
                 Log.d(TAG, "getTotal: ${coinViewModel.allCoins.value?.lastIndex}")
+                if(getTotal("gain",0) == -1){
+                    tvTotal.setText("0")
 
-                if( coinViewModel.allCoins.value?.lastIndex == -1){
-
-                    coinViewModel.addCoin(Coin(0, "","",0,0))
-                    Log.d(TAG, "getTotal2: ${coinViewModel.allCoins.value?.lastIndex}")
+                }else{
+                    tvTotal.setText(getTotal("gain",0).toString())
 
                 }
-                total.setText(getTotal("gain",0).toString()!!)
                 gainBtn.setOnClickListener(View.OnClickListener {
                     var amount = addMoney.text.toString().toInt()
-                    insertDataToDataBase(amount,"gain")
+                        insertDataToDataBase(amount,"gain")
+                })
+                spendBtn.setOnClickListener(View.OnClickListener {
+                    var amount = addMoney.text.toString().toInt()
+                    insertDataToDataBase(amount,"spend")
                 })
                 adapter.updateList(it)
             }
@@ -78,48 +85,60 @@ class MainActivity : AppCompatActivity() , CoinListAdapter.DeleteInterface {
     }
     @RequiresApi(Build.VERSION_CODES.O)
     private fun insertDataToDataBase(amount: Int, type: String) {
-        //val totalMoney: String = addMoney.getText().toString()
-        //val totalMoneyInt : Int = Integer.parseInt(totalMoney)
-        if(inputCheck(amount)){
 
-                //var total = getTotal()?.plus(amount)
-                coinViewModel.addCoin(Coin(0, LocalDateTime.now().toString(),type,amount,getTotal(type,amount)))
+        Log.d(TAG, "insertDataToDataBase: ${amount} - ${type} - ${getTotal(type,amount)}")
+        if(inputCheck(amount.toString())){
+            if(getTotal(type,amount) == -1 ){
+                if (type == "spend"){
+                    coinViewModel.addCoin(Coin(0, getDate(),type,amount,-amount))
 
-                Log.d(TAG, "insertDataToDataBase: ${amount} - ${type} - ${getTotal(type,amount)}")
+                }else{
+                    coinViewModel.addCoin(Coin(0, getDate(),type,amount,amount))
 
+                }
+                Log.d(TAG, "insertDataToDataBase: date = ${ LocalDateTime.now()}")
+
+            }else{
+                coinViewModel.addCoin(Coin(0, getDate(),type,amount,getTotal(type,amount)))
+            }
             Toast.makeText(this,"successfully",Toast.LENGTH_SHORT)
         }else{
             Toast.makeText(this,"faild",Toast.LENGTH_SHORT)
 
         }
     }
-    private fun getTotal(type: String,amount: Int): Int? {
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun getTotal(type: String, amount: Int): Int? {
 
-            Log.d(TAG, "getTotal: ${coinViewModel.allCoins.value?.lastIndex}")
+        Log.d(TAG, "getTotal: ${coinViewModel.allCoins.value?.lastIndex}")
+        if (coinViewModel.allCoins.value?.lastIndex == -1 ){
+            return -1
+        }else{
             var index = coinViewModel.allCoins.value?.lastIndex
+            var total : Int? = coinViewModel.allCoins.value?.get(index!!)?.total
             if (total != null) {
-                var total : Int? = coinViewModel.allCoins.value?.get(index!!)?.total
-                if (total != null) {
-                    if (type == "gain"){
-                        total += amount
-                        return total
-                    }else{
-                        total -= amount
-                        return total
-                    }
-                }else{
-                    total = 0
-                    getTotal(type,amount)
+                if (type == "gain"){
+                    total += amount
+                    return total
+                }else if (type == "spend"){
+                    total -= amount
+                    return total
                 }
             }
-
+        }
         return 0
     }
-    private fun inputCheck( money : Int): Boolean {
-        return !(TextUtils.isEmpty(money.toString()))
+    private fun inputCheck( money : String): Boolean {
+        return !(TextUtils.isEmpty(money))
     }
     override fun onDelete(coin: Coin) {
         coinViewModel.deleteCoin(coin)
         Toast.makeText(this,"Coin deleted successfully", Toast.LENGTH_LONG).show()
+    }
+    @RequiresApi(Build.VERSION_CODES.N)
+    fun getDate():String{
+        val simpleDate = SimpleDateFormat("dd/M/ hh:mm a")
+        val currentDate = simpleDate.format(Date())
+        return currentDate
     }
 }
